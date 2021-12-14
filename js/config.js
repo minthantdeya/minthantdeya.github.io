4c55dac372b82316eed49d68a9f95c54cd4b57e7
minthantdeya <95972407+minthantdeya@users.noreply.github.com>
2021-12-14 22:48:59 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,10 @@
+let site_detail = {
+    'site_name': 'Deya',
+    'tagline': 'A site script for static website with dynamic data',
+    'site_home': 'https://minthantdeya.github.io',
+    'blog_home': 'https://minthantdeya.github.io/itscookiechannel',
+    'products': 'products.html'
+}
+
+export {site_detail}
 

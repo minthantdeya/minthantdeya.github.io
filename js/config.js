24ddd7019b81375b2e7acf794678c7908b780f3b
minthantdeya <95972407+minthantdeya@users.noreply.github.com>
2021-12-14 23:12:35 +0630

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,16 @@
 let site_detail = {
     'site_name': {
-        'tc': 'MinthantDeya',
+        'tc': 'LovelyMyanmar',
         'url': 'https://minthantdeya.github.io'
     },
     'site_home': {
         'tc': 'Home',
-        'url': 'https://minthantdeya.github.io/itscookiechannel'
+        'url': 'https://minthantdeya.github.io/springmoni'
     },
     'products': {
         'tc': 'Our Products',
         'url': 'https://minthantdeya.github.io/itscookiechannel/products/index.html'
     }
 }
+
+export {site_detail}
